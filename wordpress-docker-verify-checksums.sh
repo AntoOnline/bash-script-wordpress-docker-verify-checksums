59a559f8cfd7b7e537494b6cbb24558625dfea12
Anthony Mastoroudes <anthony.mast@gmail.com>
2023-05-15 05:13:11 +1000

    Download once then copy to containers and update the way it displays.

--- wordpress-docker-verify-checksums.sh
+++ wordpress-docker-verify-checksums.sh
@@ -15,32 +15,26 @@ if [[ -z $SLACK_WEBHOOK_URL ]]; then
     SLACK_ENABLED=false
 fi
 
+# Download wp-cli.phar to /tmp directory
+echo "Downloading wp-cli.phar..."
+curl -o /tmp/wp-cli.phar https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+
 # List all container names associated with the wordpress image
 container_names=$(docker ps -aqf "ancestor=wordpress" --format "{{.Names}}")
 
-# Loop through each container name and run the wp core verify-checksums command
+# Loop through each container name and copy wp-cli.phar
 for container_name in $container_names; do
     echo "Checking container: $container_name"
-    # Check if wp-cli is installed in the container
-    if ! docker exec $container_name which wp > /dev/null; then
-        echo "wp-cli not found in container: $container_name"
-        # Install wp-cli if INSTALL_WP_CLI is true
-        if [[ $INSTALL_WP_CLI == true ]]; then
-            echo "Installing wp-cli in container: $container_name"
-            docker exec $container_name sh -c "curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && chmod +x wp-cli.phar && mv wp-cli.phar /usr/local/bin/wp"
-        fi
+    echo "- Installing latest wp-cli in container: $container_name"
+    docker cp /tmp/wp-cli.phar $container_name:/usr/local/bin/wp
+    docker exec $container_name chmod +x /usr/local/bin/wp
+    # Verify checksums using wp-cli
+    if ! docker exec --workdir /var/www/html/ --user www-data $container_name wp core verify-checksums; then
+        echo "- Checksum verification failed for container: $container_name"
         # Send Slack notification if enabled
         if [[ $SLACK_ENABLED == true ]]; then
-            curl -X POST -H 'Content-type: application/json' --data '{"text":"wp-cli not found in container: '$container_name'"}' $SLACK_WEBHOOK_URL
-        fi
-    else
-        # Verify checksums using wp-cli
-        if ! docker exec --workdir /var/www/html/ --user www-data $container_name wp core verify-checksums; then
-            echo "Checksum verification failed for container: $container_name"
-            # Send Slack notification if enabled
-            if [[ $SLACK_ENABLED == true ]]; then
-                curl -X POST -H 'Content-type: application/json' --data '{"text":"Checksum verification failed for container: '$container_name'"}' $SLACK_WEBHOOK_URL
-            fi
+            curl -X POST -H 'Content-type: application/json' --data '{"text":"Checksum verification failed for container: '$container_name'"}' $SLACK_WEBHOOK_URL
         fi
     fi
+    echo ""
 done
