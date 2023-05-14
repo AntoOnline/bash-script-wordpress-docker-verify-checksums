71e8b52b4ffce845c181d80b04cd4abb2932e72b
Anthony Mastoroudes <anthony.mast@gmail.com>
2023-05-15 05:29:06 +1000

    Added plugin verification

--- wordpress-docker-verify-checksums.sh
+++ wordpress-docker-verify-checksums.sh
@@ -25,16 +25,27 @@ container_names=$(docker ps -aqf "ancestor=wordpress" --format "{{.Names}}")
 # Loop through each container name and copy wp-cli.phar
 for container_name in $container_names; do
     echo "Checking container: $container_name"
+
     echo "- Installing latest wp-cli in container: $container_name"
     docker cp /tmp/wp-cli.phar $container_name:/usr/local/bin/wp
     docker exec $container_name chmod +x /usr/local/bin/wp
-    # Verify checksums using wp-cli
+    # Verify WordPress core using wp-cli
     if ! docker exec --workdir /var/www/html/ --user www-data $container_name wp core verify-checksums; then
-        echo "- Checksum verification failed for container: $container_name"
+        echo "- WordPress Core Checksum verification failed for container: $container_name"
         # Send Slack notification if enabled
         if [[ $SLACK_ENABLED == true ]]; then
-            curl -X POST -H 'Content-type: application/json' --data '{"text":"Checksum verification failed for container: '$container_name'"}' $SLACK_WEBHOOK_URL
+            curl -X POST -H 'Content-type: application/json' --data '{"text":"WordPress Core Checksum verification failed for container: '$container_name'"}' $SLACK_WEBHOOK_URL
         fi
     fi
+
+    # Verify WordPress plugins using wp-cli
+    if ! docker exec --workdir /var/www/html/ --user www-data $container_name wp plugin verify-checksums --all; then
+        echo "- WordPress Plugin Checksum verification failed for container: $container_name"
+        # Send Slack notification if enabled
+        if [[ $SLACK_ENABLED == true ]]; then
+            curl -X POST -H 'Content-type: application/json' --data '{"text":"WordPress Plugin Checksum verification failed for container: '$container_name'"}' $SLACK_WEBHOOK_URL
+        fi
+    fi
+
     echo ""
 done
